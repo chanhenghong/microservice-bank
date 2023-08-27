917afffd3848180ddfdf5a0acf51cbb9158cc8c6
Chanheng Hong <chanheng2002@gmail.com>
2023-08-27 19:10:27 +0700

    Config server docker compose

---
+++ account/src/main/java/com/chanheng/bank/account/controller/ConfigController.java
@@ -0,0 +1,29 @@
+package com.chanheng.bank.account.controller;
+
+import com.chanheng.bank.account.config.AccountServiceConfig;
+import com.chanheng.bank.account.property.Properties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+@RestController
+@RequestMapping("api/config")
+public class ConfigController {
+    @Autowired
+    private AccountServiceConfig serviceConfig;
+
+    @GetMapping("/properties")
+    public String getProperties() throws JsonProcessingException {
+        ObjectWriter objectWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(serviceConfig.getMsg(), serviceConfig.getBuildVersion(),
+                serviceConfig.getMailDetails(), serviceConfig.getActiveBranches());
+        String json = objectWriter.writeValueAsString(properties);
+        return json;
+    }
+
+}
